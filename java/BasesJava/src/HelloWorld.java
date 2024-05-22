665885a5f4b3e7d55cf966b288ea1f26d8127183
JDDeveloper1 <jodasos03@gmail.com>
2024-05-22 13:01:55 +0200

    Java Sessions

---
+++ java/BasesJava/src/HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hola Juli \uD83D\uDE0A");
+    }
+}
+
+/*
+Primitives
+int, short, byte, long
+boolean, float, double.
+
+References an
+Objets
+String, array, etc.*/
